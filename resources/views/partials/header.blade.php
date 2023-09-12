01caf8ded2251c7506b4cc2154e5d33eaae30368
Camilo Zapata <camilozapata129@gmail.com>
2023-09-11 20:26:25 -0500

    feat: add header and footer partials

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,6 @@
+<header class="bg-body-tertiary">
+    <div class="container py-3 d-flex justify-content-between align-items-center">
+        <span class="fs-4">Prison Architect Calculator</span>
+        <button type="button" class="btn btn-outline-light">Support</button>
+    </div>
+</header>
