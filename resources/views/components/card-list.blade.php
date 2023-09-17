e2c620b638644fceaa2cd93218da7a522b455aba
Camilo Zapata <camilozapata129@gmail.com>
2023-09-17 02:17:43 -0500

    style: auto width. add fs-5 class to badge

--- resources/views/components/card-list.blade.php
+++ resources/views/components/card-list.blade.php
@@ -1,7 +1,7 @@
 <div class="col-6 d-flex justify-content-end" style="padding-right: 0%; padding-left: 0%">
-    <div class="card border-secondary" style="width: 18rem;">
+    <div class="card ms-3 border-secondary w-100">
         <div class="card-header text-center">
-            <span class="fs-5">{{ $title }}</span>
+            <span class="fs-6">{{ $title }}</span>
         </div>
         <ul class="list-group list-group-flush">
             {{ $slot }}
