5f103e47a892c7c631be19e40b21b10bea21b959
Camilo Zapata <camilozapata129@gmail.com>
2023-09-17 00:21:33 -0500

    style: modify tab-list navigation button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,17 @@
 <x-app>
-    <div class="text-center">
-        <span class="fs-2">{{ __('Total number of prisoners:') }}</span>
-        <div class="d-flex justify-content-center">
-            <input type="number" class="form-control form-control-lg w-50">
-        </div>
-    </div>
+    <livewire:prisoners-input />
 
     <div class="pt-3">
         <nav>
-            <div class="nav nav-tabs justify-content-center" role="tablist">
+            <div class="nav nav-tabs justify-content-center border-0" role="tablist">
                 <x-tab-button active="true" target="feeding">{{ __('Kitchen / Canteen') }}</x-tab-button>
-                <x-tab-button active="false" target="laundry">{{ __('Laundry') }}</x-tab-button>
-                <x-tab-button active="false" target="workshop">{{ __('Workshop') }}</x-tab-button>
+                {{-- <x-tab-button active="false" target="laundry">{{ __('Laundry') }}</x-tab-button>
+                <x-tab-button active="false" target="workshop">{{ __('Workshop') }}</x-tab-button> --}}
             </div>
         </nav>
 
-        <div class="tab-content">
-            <x-tab-pane active="true" target="feeding">1</x-tab-pane>
-            <x-tab-pane active="false" target="laundry">2</x-tab-pane>
-            <x-tab-pane active="false" target="workshop">3</x-tab-pane>
+        <div class="tab-content px-3 rounded bg-dark bg-gradient bg-opacity-10">
+            <livewire:feeding />
         </div>
     </div>
 </x-app>
