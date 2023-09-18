cac127d4947c3d58c63fa541290a526699f57d99
Camilo Zapata <camilozapata129@gmail.com>
2023-09-18 00:58:15 -0500

    feat: localization configuration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,15 +7,36 @@
 
     <title>{{ config('app.name') }}</title>
 
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     @livewireStyles
 </head>
 
 <body class="d-flex flex-column h-100">
+
     @include('partials.header')
 
     <main class="container py-3">
-        {{ $slot }}
+
+        <div class="dropdown position-fixed bottom-0 end-0 me-3 bd-mode-toggle" style="margin-bottom: 4rem">
+            <button class="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-gear-fill" style="font-size: 16px"></i>
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item text-center">{{ __('Set site language:') }}</a></li>
+                <li class="d-flex align-items-center">
+                    <img src="images/icons/estados-unidos.png" class="ms-2" width="16px">
+                    <a href="{{ route('locale', 'en') }}" class="dropdown-item ps-1">{{ __('English') }}</a>
+                </li>
+                <li class="d-flex align-items-center">
+                    <img src="images/icons/mexico.png" class="ms-2" width="16px">
+                    <a href="{{ route('locale', 'es') }}" class="dropdown-item ps-1">{{ __('Spanish') }}</a>
+                </li>
+            </ul>
+        </div>
+
+        {{-- {{ $slot }} --}}
     </main>
 
     @include('partials.footer')
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+
+Route::get('/locale/{locale}', function (string $locale) {
+    if (! in_array($locale, ['en', 'es', 'fr'])) {
+        abort(400);
+    }
+
+    session()->put('locale', $locale);
+
+    return back();
+})->name('locale');
