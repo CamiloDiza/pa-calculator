83b288f08140d4fdb9fe9393d1635046792c5556
Camilo Zapata <camilozapata129@gmail.com>
2023-09-18 00:59:40 -0500

    feat: add copyright year with date function

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer mt-auto py-3 bg-body-tertiary">
     <div class="container d-flex justify-content-between">
-        <span>&copy; 2023 {{ config('app.name') }}. All rights reserved.</span>
+        <span>&copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.</span>
         <span>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</span>
     </div>
 </footer>
